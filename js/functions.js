467adeb3c99231898048345c91f40c60fc45ecc9
Anderson Innocencio Carvalho <118061362+andersoninn@users.noreply.github.com>
2023-04-02 10:58:38 +0100

    .

--- js/functions.js
+++ js/functions.js
@@ -8,51 +8,76 @@
 // FIXED BILLS - INPUTS AND OUTPUTS
 var rent = document.querySelector('#rent1')
 
-rent.addEventListener('change', ()=>{
-    let outputR = rent.value / 3 
+    rent.addEventListener('change', ()=>{
+        var outputR = rent.value / 3 
 
-    document.querySelector('#output-rent').innerHTML = outputR.toFixed(2)
+        document.querySelector('#output-rent').innerHTML = outputR.toFixed(2)
 })
 
 var electricity = document.querySelector('#electricity1')
 
-electricity.addEventListener('change', ()=>{
-    let outputE = electricity.value / 3
+    electricity.addEventListener('change', ()=>{
+        var outputE = electricity.value / 3
 
-    document.querySelector('#output-electricity').innerHTML = outputE.toFixed(2)
+        document.querySelector('#output-electricity').innerHTML = outputE.toFixed(2)
 })
 
 var water = document.querySelector('#water1')
 
-water.addEventListener('change', ()=>{
-    let outputW = water.value / 3
+    water.addEventListener('change', ()=>{
+        var outputW = water.value / 3
 
-    document.querySelector('#output-water').innerHTML = outputW.toFixed(2)
+        document.querySelector('#output-water').innerHTML = outputW.toFixed(2)
 })
 
 var sewage = document.querySelector('#sewage1')
 
-sewage.addEventListener('change', ()=>{
-    let outputS = sewage.value / 3
+    sewage.addEventListener('change', ()=>{
+        var outputS = sewage.value / 3
 
-    document.querySelector('#output-sewage').innerHTML = outputS.toFixed(2)
+        document.querySelector('#output-sewage').innerHTML = outputS.toFixed(2)
 })
 
-// FIXED BILLS INPUT AND OUTPUT
+// INDIVIDUAL BILLS INPUT AND OUTPUT
 var nos = document.querySelector('#nos1')
 
-nos.addEventListener('change', ()=>{
+    nos.addEventListener('change', ()=>{
        alert(nos.value)
  })
 
 var gadgets = document.querySelector('#gadgets1')
 
- gadgets.addEventListener('change', ()=>{
-    alert(gadgets.value)
+    gadgets.addEventListener('change', ()=>{
+        alert(gadgets.value)
  })
 
- var Tand = document.querySelector('#t-and1')
+var Tand = document.querySelector('#t-and1')
 
- Tand.addEventListener('change', ()=>{
-    alert(Tand.value)
+    Tand.addEventListener('change', ()=>{
+        alert(Tand.value)
  })
+
+var Tyas = document.querySelector('#t-yas1')
+ 
+    Tyas.addEventListener('change', ()=>{
+        alert(Tyas.value)
+ })
+
+var Tvitor = document.querySelector('#t-vitor1')
+
+    Tvitor.addEventListener('change', ()=>{
+        alert(Tvitor.value)
+    })
+
+var Tsandra = document.querySelector('#t-sandra1')
+
+    Tsandra.addEventListener('change', ()=>{
+        alert(Tsandra.value)
+        
+    })
+
+
+
+    
+
+    
