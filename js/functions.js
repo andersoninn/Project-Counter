200f0e7b99fb827258c9e9885f48aab735ff4971
Anderson Innocencio Carvalho <118061362+andersoninn@users.noreply.github.com>
2023-04-11 21:10:23 +0100

    new style

--- js/functions.js
+++ js/functions.js
@@ -6,80 +6,113 @@
 }
 
 // FIXED BILLS - INPUTS AND OUTPUTS
-var rent = document.querySelector('#rent1')
+var ValueBills = document.querySelectorAll('.fixed-bills input')
+// console.log (ValueBills)
+var output1 = document.querySelectorAll('#output')
+console.log(output1)
 
-    rent.addEventListener('change', ()=>{
-        var outputR = rent.value / 3 
 
-        document.querySelector('#output-rent').innerHTML = outputR.toFixed(2)
-})
+ValueBills.forEach(element => adicionar(){
+     function adicionar(){ 
+        output1.addEventListener('change', ()=>{ 
+            output1 = element.NextSibling.value / 3
+        })
+    }
+    console.log(output1)
+    
+ });
 
-var electricity = document.querySelector('#electricity1')
 
-    electricity.addEventListener('change', ()=>{
-        var outputE = electricity.value / 3
 
-        document.querySelector('#output-electricity').innerHTML = outputE.toFixed(2)
-})
 
-var water = document.querySelector('#water1')
 
-    water.addEventListener('change', ()=>{
-        var outputW = water.value / 3
+// var rent = document.querySelector('#rent1')
 
-        document.querySelector('#output-water').innerHTML = outputW.toFixed(2)
-})
+//     rent.addEventListener('change', ()=>{
+//         var outputR = rent.value / 3 
 
-var sewage = document.querySelector('#sewage1')
+//         document.querySelector('#output-rent').innerHTML = outputR.toFixed(2)
+// })
 
-    sewage.addEventListener('change', ()=>{
-        var outputS = sewage.value / 3
+// var electricity = document.querySelector('#electricity1')
 
-        document.querySelector('#output-sewage').innerHTML = outputS.toFixed(2)
-})
+//     electricity.addEventListener('change', ()=>{
+//         var outputE = electricity.value / 3
 
-// INDIVIDUAL BILLS INPUT AND OUTPUT
-var nos = document.querySelector('#nos1')
+//         document.querySelector('#output-electricity').innerHTML = outputE.toFixed(2)
+// })
 
-    nos.addEventListener('change', ()=>{
-       alert(nos.value)
- })
+// var water = document.querySelector('#water1')
 
-var gadgets = document.querySelector('#gadgets1')
+//     water.addEventListener('change', ()=>{
+//         var outputW = water.value / 3
 
-    gadgets.addEventListener('change', ()=>{
-        alert(gadgets.value)
- })
+//         document.querySelector('#output-water').innerHTML = outputW.toFixed(2)
+// })
 
-var Tand = document.querySelector('#t-and1')
+// var sewage = document.querySelector('#sewage1')
 
-    Tand.addEventListener('change', ()=>{
-        alert(Tand.value)
- })
+//     sewage.addEventListener('change', ()=>{
+//         var outputS = sewage.value / 3
 
-var Tyas = document.querySelector('#t-yas1')
+//         document.querySelector('#output-sewage').innerHTML = outputS.toFixed(2)
+// })
+
+// // INDIVIDUAL BILLS INPUT AND OUTPUT
+// var nos = document.querySelector('#nos1')
+//     // nos.addEventListener('change', ()=>{
+//     //     let span = document.createElement('span')
+//     //     span.innerHTML = 'ok'
+//     //     span.setAttribute('class', 'check')
+//     //     let check1 = document.querySelector('#nos1').nextElementSibling
+
+//     //     if(document.querySelector('#nos1').value==''){ 
+//     //         check1.remove()
+//     //     }
+//     //     alert(check1)
+//     //     if(check1==null){
+//     //         document.querySelector('#nos1').parentNode.appendChild(span)
+//     //     }
+
+
+
+//     // })
+
+// var gadgets = document.querySelector('#gadgets1')
+
+// //     gadgets.addEventListener('change', ()=>{
+// //         alert(gadgets.value)
+// //  })
+
+// var Tand = document.querySelector('#t-and1')
+
+// //     Tand.addEventListener('change', ()=>{
+// //         alert(Tand.value)
+// //  })
+
+// var Tyas = document.querySelector('#t-yas1')
  
-    Tyas.addEventListener('change', ()=>{
-        alert(Tyas.value)
- })
+// //     Tyas.addEventListener('change', ()=>{
+// //         alert(Tyas.value)
+// //  })
 
-var Tvitor = document.querySelector('#t-vitor1')
+// var Tvitor = document.querySelector('#t-vitor1')
 
-    Tvitor.addEventListener('change', ()=>{
-        alert(Tvitor.value)
-    })
+//     // Tvitor.addEventListener('change', ()=>{
+//     //     alert(Tvitor.value)
+//     // })
 
-var Tsandra = document.querySelector('#t-sandra1')
+// var Tsandra = document.querySelector('#t-sandra1')
 
-    Tsandra.addEventListener('change', ()=>{
-        alert(Tsandra.value)
+//     // Tsandra.addEventListener('change', ()=>{
+//     //     alert(Tsandra.value)
         
-    })
+//     // })
 
 
 
     
 
-function addNote(){
+// function addNote(){
     
-} 
+// } 
