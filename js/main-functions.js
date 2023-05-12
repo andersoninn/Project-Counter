e1f14cf0d90f9c23cf80413490cc02919bfba88a
Anderson Innocencio Carvalho <118061362+andersoninn@users.noreply.github.com>
2023-05-12 19:55:03 +0100

    Calculation Functions

--- js/main-functions.js
+++ js/main-functions.js
@@ -1,40 +1,25 @@
 
+/*************************************
+** FIXED BILLS - INPUTS AND OUTPUTS **
+*************************************/
 
-// FIXED BILLS - INPUTS AND OUTPUTS
 var fixedBills = document.querySelectorAll('.fixed-bills input')
 // console.log (fixedBills)
 
 fixedBills.forEach((item) => {
-    
-    item.addEventListener('change', (element)=>{
-        let finalValue = Number(element.target.value) /3
+
+    item.addEventListener('change', (element) => {
+        let finalValue = Number(element.target.value) / 3
         element.target.parentNode.nextElementSibling.innerHTML = finalValue.toFixed(2)
 
-       })
+    })
 });
 
-   
+/****************************************
+*** INDIVIDUAL BILLS INPUT AND OUTPUT ***
+****************************************/
 
-// INDIVIDUAL BILLS INPUT AND OUTPUT
-
-//SCOPE OF LOGIC
-//Create a new variable(var1) that receives the value of section input. -ok
-
-//Use forEach, for each item of the variable and execute the functin addEventListiner('change, (element) =>{}) that will generation the fuction 'element' through the arrow function - ok
-
-//Declarete the new variable (var2) that receive number(element.target.value) - ok
-
-//Do the logical test to see if valur is > 0. -ok
-
-//If Number(element.target.value) > 0, inserted the material icon -ok
-
-//Else alert('Digite um montante válido (maior que zero)')-ok
-
-//Test if the material icon has been inserted, if yes, do nothing. 
-//Else, add the material icon. -ok
-
-let individualBills = document.querySelectorAll('.individual-bills input')
-// console.log(individualBills)
+var individualBills = document.querySelectorAll('.individual-bills input')
 
 individualBills.forEach((item) => {
     // console.log(item)
@@ -46,29 +31,35 @@ individualBills.forEach((item) => {
             span.innerHTML = ` <span class="material-symbols-outlined">
             check </span>`
             // element.target.parentNode.insertBefore(span,  element.target.nextElementSibling)
-            
-                if(element.target.parentNode.children[1] == undefined){
-                    // alert('to adds')
-                    element.target.after(span)
-                } else{ 
-                    // alert('different')
-                }
-        }else{
+
+            if (element.target.parentNode.children[1] == undefined) {
+                // alert('to adds')
+                element.target.after(span)
+            } else {
+                // alert('different')
+            }
+        } else {
             alert('Digite um montate válido (maior que zero)')
         }
     })
 }
 )
-    //BUTTON
-    let addNote = document.querySelector('.button').firstElementChild
-    // console.log (addNote)
 
-    addNote.addEventListener('click', ()=> {
-        // console.log (note)
-        let patherSection = document.querySelectorAll('section')
-        var newSection = document.createElement('section')
-        newSection.setAttribute('class', 'supermarket box')
-        newSection.innerHTML += `
+
+/****************************************
+*********** SUPERMARKET BILLS ***********
+****************************************/
+
+//BUTTON "ADD-NEW-NOTE"
+let addNote = document.querySelector('.button').firstElementChild
+// console.log (addNote)
+
+addNote.addEventListener('click', () => {
+    // console.log (note)
+    let patherSection = document.querySelectorAll('section')
+    var newSection = document.createElement('section')
+    newSection.setAttribute('class', 'supermarket box')
+    newSection.innerHTML += `
         <div>
             <label for="total">Total:</label>
             <input type="number" name="total" id="" style="width:80px">
@@ -85,121 +76,125 @@ individualBills.forEach((item) => {
                     delete </span>
                 </button>
         </div>`
-        patherSection[2].appendChild(newSection)
-        evButtom();
-    });
+    patherSection[2].appendChild(newSection)
     evButtom();
-    
-    // REMOVE
-    function evButtom(){
-        
-            document.querySelectorAll('.supermarket button').forEach(elemBtn =>{
-                elemBtn.addEventListener('click', (e)=>{
-                    if(e.target.classList != ''){
-                        var btnElement = e.target.parentNode
-   
-                    } else{
-                        var btnElement = e.target
-                    }
+});
+evButtom();
 
-                    let tamanhoSection = document.querySelectorAll('section button') 
-                        if(tamanhoSection.length > 3){
-                        
-                        btnElement.parentNode.parentNode.remove() 
-            
-                        } else{
-                           document.querySelectorAll('.supermarket input').forEach(input =>{
-                            input.value = '0'
-                           })
-                    }
+// BUTTON-REMOVE
+function evButtom() {
+
+    document.querySelectorAll('.supermarket button').forEach(elemBtn => {
+        elemBtn.addEventListener('click', (e) => {
+            if (e.target.classList != '') {
+                var btnElement = e.target.parentNode
+
+            } else {
+                var btnElement = e.target
+            }
+
+            let tamanhoSection = document.querySelectorAll('section button')
+            if (tamanhoSection.length > 3) {
+
+                btnElement.parentNode.parentNode.remove()
+
+            } else {
+                document.querySelectorAll('.supermarket input').forEach(input => {
+                    input.value = '0'
                 })
-            })
-        }
+            }
+        })
+    })
+}
+
+// SUBMIT-BUTTON - CALCULATION FUNCTION
+document.querySelector('[type=submit]').addEventListener('click', () => {
+
+    var andIndBills = 0;
+    var yasIndBills = 0;
+    var vitorIndBills = 0;
+
+    //FIXED-BILLS
+    let finalValue1 = 0;
+    fixedBills.forEach((element) => {
+        finalValue1 += parseFloat(element.value) / 3;
+    });
     
+    //INDIVIDUAL-BILLS
+    let gadgets = Number(individualBills[0].value) / 2 
+    let telAnd = Number(individualBills[1].value)
+    let telYas = Number(individualBills[2].value)
+    let telVitor = Number(individualBills[3].value)
+    let telSandra = Number(individualBills[4].value) / 2
+    let Spotify = 2
+    
+    andIndBills = gadgets + telAnd + telSandra + Spotify;
+    yasIndBills = gadgets + telYas + telSandra + Spotify;
+    vitorIndBills = telVitor + Spotify;
+    // console.log(andFinal);
+    // console.log(yasFinal);
+    // console.log(vitorFinal);
+    
+    //SUPERMARKET-BILLS
 
-// ************************************************
-// ****************    OLD CODE    ****************
-// ************************ ***********************    
-//FIXED BILLS
+        let supMTotal = document.querySelectorAll('[name=total]');
+            supMTotalValue = 0;
+            supMTotal.forEach((note) => { 
+            supMTotalValue += Number(note.value);
+    });
+            // console.log(`o valor  total é ${supMTotalValue}`);
+       
+        let supMVitor = document.querySelectorAll('[name=vitor]');
+            supMVitorValue = 0;
+            supMVitor.forEach((note) => {
+                supMVitorValue += Number(note.value)
+        });
+            // console.log(`o valor do Vitor é ${supMVitorValue}`);
 
-// var rent = document.querySelector('#rent1')
+        let supMAndYas = document.querySelectorAll('[name=andYas]');
+            supMAndYasValue = 0;
+            supMAndYas.forEach((note) =>{
+                supMAndYasValue += Number(note.value)  
+            });
+            // console.log(`o valor And/Yas é ${supMAndYasValue}`);
 
-//     rent.addEventListener('change', ()=>{
-//         var outputR = rent.value / 3 
-//         document.querySelector('#output-rent').innerHTML = outputR.toFixed(2)
-// })
+     var vitorSupMFinal = (supMTotalValue - supMVitorValue - supMAndYasValue)/3 + supMVitorValue
+    //  console.log(`O valor do Vitor é ${vitorSupMFinal.toFixed(2)}`);
 
-// var electricity = document.querySelector('#electricity1')
+    var AndYasSupMFinal = ((supMTotalValue - supMAndYasValue - supMVitorValue)/3)*2 + supMAndYasValue
+    // console.log(`o valor And/Yas é ${AndYasSupMFinal.toFixed(2)}`);
 
-//     electricity.addEventListener('change', ()=>{
-//         var outputE = electricity.value / 3
-//         document.querySelector('#output-electricity').innerHTML = outputE.toFixed(2)
-// })
+    var finalAndYas = (finalValue1)*2 + andIndBills + yasIndBills + AndYasSupMFinal;
+    // console.log(finalAndYas.toFixed(2))
+    
+    var finalVitor = finalValue1 + vitorIndBills + vitorSupMFinal
+    // console.log(finalVitor.toFixed(2))
 
-// var water = document.querySelector('#water1')
-
-//     water.addEventListener('change', ()=>{
-//         var outputW = water.value / 3
-//         document.querySelector('#output-water').innerHTML = outputW.toFixed(2)
-// })
-
-// var sewage = document.querySelector('#sewage1')
-
-//     sewage.addEventListener('change', ()=>{
-//         var outputS = sewage.value / 3
-
-//         document.querySelector('#output-sewage').innerHTML = outputS.toFixed(2)
-// })
-
-// INDIVIDUAL BILLS INPUT AND OUTPUT
-// var nos = document.querySelector('#nos1')
-//     nos.addEventListener('change', ()=>{
-//         let span = document.createElement('span')
-//         span.innerHTML = 'ok'
-//         span.setAttribute('class', 'check')
-//         let check1 = document.querySelector('#nos1').nextElementSibling
-
-//         if(document.querySelector('#nos1').value==''){ 
-//             check1.remove()
-//         }
-//         alert(check1)
-//         if(check1==null){
-//             document.querySelector('#nos1').parentNode.appendChild(span)
-//         }
-//     }
-
-// var gadgets = document.querySelector('#gadgets1')
-
-//         gadgets.addEventListener('change', ()=>{
-//             alert(gadgets.value)
-//      })
-
-// var Tand = document.querySelector('#t-and1')
-
-//         Tand.addEventListener('change', ()=>{
-//              alert(Tand.value)
-//      })
-
-// var Tyas = document.querySelector('#t-yas1')
+    alert(`
+OS VALORES TOTAIS SÃO:
+    Anderson e Yasmim: ${finalAndYas.toFixed(2)}
+    Vitor: ${finalVitor.toFixed(2)}
  
-//         Tyas.addEventListener('change', ()=>{
-//             alert(Tyas.value)
-//      })
+SEGUE A BAIXO A BASE DE CÁLCULOS:
+CONTAS COMUNS: 
+    Anderson:  ${finalValue1.toFixed(2)} 
+    Yasmim: ${finalValue1.toFixed(2)} 
+    Vitor: ${finalValue1.toFixed(2)} 
 
-// var Tvitor = document.querySelector('#t-vitor1')
+CONTAS INDIVIDUAIS: 
+    Anderson: ${andIndBills.toFixed(2)} 
+    Yasmim: ${yasIndBills.toFixed(2)}
+    Vitor: ${vitorIndBills.toFixed(2)}
 
-//     Tvitor.addEventListener('change', ()=>{
-//         alert(Tvitor.value)
-//     })
-
-// var Tsandra = document.querySelector('#t-sandra1')
-
-//     Tsandra.addEventListener('change', ()=>{
-//         alert(Tsandra.value)
-        
-//     })
+MERCADO
+    Anderson e Yasmim: ${AndYasSupMFinal.toFixed(2)}
+    Vitor: ${vitorSupMFinal.toFixed(2)}
+    `);
 
 
-// function addNote(){
+/**********************
+***** FINAL VALUE *****
+***********************/  
     
-// } 
+    });
+
